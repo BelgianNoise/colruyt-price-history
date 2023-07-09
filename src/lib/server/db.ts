fa3755f5e406b2ef5c51cc4d9643e9614262d24c
Arthur Joppart <arthur@digita.ai>
2023-07-09 22:08:31 +0200

    fix: database connections

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,7 +1,7 @@
-import { Pool, type PoolClient } from 'pg';
+import { Pool } from 'pg';
 import { DB_HOST, DB_PORT, DB_PASSWORD, DB_USERNAME, DB_DATABASE } from '$env/static/private';
 
-const pool = new Pool({
+export const dbPool = new Pool({
   host: DB_HOST,
   port: Number(DB_PORT),
   user: DB_USERNAME,
@@ -11,5 +11,3 @@ const pool = new Pool({
   max: 2,
   idleTimeoutMillis: 60000,
 });
-
-export const connectToDB = async (): Promise<PoolClient> => await pool.connect();
--- src/routes/api/search/+server.ts
+++ src/routes/api/search/+server.ts
@@ -1,5 +1,5 @@
 import { parseToProduct, type Product } from "$lib/models/product";
-import { connectToDB } from "$lib/server/db";
+import { dbPool } from "$lib/server/db";
 import { json, type RequestEvent } from "@sveltejs/kit";
 
 export async function GET(event: RequestEvent): Promise<Response> {
@@ -7,14 +7,12 @@ export async function GET(event: RequestEvent): Promise<Response> {
   const query = event.url.searchParams.get('query');
   if (!query) return json([], { status: 400 });
 
-  const client = await connectToDB();
-
-  const result = await client.query(`
+  const result = await dbPool.query(`
     SELECT *
     FROM products.product
     WHERE long_name ILIKE '%' || $1 || '%'
   `, [ query ]);
-  
+
   const products: Product[] = result.rows.map((row) => parseToProduct(row));
 
   return json(products);
