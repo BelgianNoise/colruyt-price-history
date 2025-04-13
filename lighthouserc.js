077b607bd7f451ed62500ab2c57e05d99c63ef82
Arthur Joppart <arthur.joppart@gmail.com>
2025-04-13 22:02:07 +0200

    ci: add lighthouse workflow

---
+++ lighthouserc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  ci: {
+    collect: {
+      url: ['http://localhost:4173/'],
+      startServerCommand: '', // We're starting the server in the GitHub Action
+      numberOfRuns: 3,
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+    assert: {
+      preset: 'lighthouse:recommended',
+      assertions: {
+        'categories:performance': ['warn', {minScore: 0.7}],
+        'categories:accessibility': ['warn', {minScore: 0.8}],
+        'categories:best-practices': ['warn', {minScore: 0.8}],
+        'categories:seo': ['warn', {minScore: 0.8}],
+      },
+    },
+  },
+};
