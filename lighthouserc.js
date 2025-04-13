2d93b6cb7dfd917b5a2b208ec1df4286ac48e073
Arthur Joppart <arthur.joppart@gmail.com>
2025-04-13 22:51:40 +0200

    ci: run lighthouse against deployed app

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,20 +1,17 @@
-module.exports = {
-  ci: {
-    collect: {
-      url: ['http://localhost:4173/'],
-      startServerCommand: '', // We're starting the server in the GitHub Action
-    },
-    upload: {
-      target: 'temporary-public-storage',
-    },
-    assert: {
-      preset: 'lighthouse:recommended',
-      assertions: {
-        'categories:performance': ['error', {minScore: 0.9}],
-        'categories:accessibility': ['error', {minScore: 0.9}],
-        'categories:best-practices': ['error', {minScore: 0.9}],
-        'categories:seo': ['error', {minScore: 0.9}],
-      },
+export const ci = {
+  collect: {
+    url: ['https://colruyt-prijzen.nasaj.be/'],
+  },
+  upload: {
+    target: 'temporary-public-storage',
+  },
+  assert: {
+    preset: 'lighthouse:recommended',
+    assertions: {
+      'categories:performance': ['error', { minScore: 0.9 }],
+      'categories:accessibility': ['error', { minScore: 0.9 }],
+      'categories:best-practices': ['error', { minScore: 0.9 }],
+      'categories:seo': ['error', { minScore: 0.9 }],
     },
   },
 };
